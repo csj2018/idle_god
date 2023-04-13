b7942e0f3540cb6f4d13bcf6188d030471867ce1
jjg <506300081@qq.com>
2023-04-14 00:57:56 +0800

    fix bug

--- src/cls/World.py
+++ src/cls/World.py
@@ -9,7 +9,7 @@ class World():
         self.门派 = ['\033[32m凌霄派\033[0m','\033[32m圣火门\033[0m','\033[32m玄天洞\033[0m','\033[32m魔门\033[0m','\033[32m散修\033[0m']
         self.地点 = ['古战场']
         self.事件 = 0
-        self.随机事件权重 = [['加人',12],['恩怨',10],['奇遇',5],['帮战',10],['',10]]
+        self.随机事件权重 = [['加人',12],['恩怨',10],['奇遇',5],['帮战',1],['',100]]
         self.随机事件分段 = []
         self.run = 1
         self.time = [0, 1]
@@ -38,13 +38,13 @@ class World():
             tmp.门派 = random.choice(self.门派)
             tmp.姓名 = f'\033[35m{i}\033[0m'
             tmp.天命 = 1
-            tmp.转世 = 5 #TODO
+            tmp.转世 = 1 #TODO
             tmp.update()
             self.人物.append(tmp)
             p += f"大造化将{tmp.姓名}投入这一方小世界中\n"
         return p
     def new_mp(self):
-        a = random.choice(['星云','天河','昆仑','崆峒','元华','墨佬'])
+        a = random.choice(['星云','天河','昆仑','崆峒','元华','墨佬','化生','乾坤'])
         b = random.choice(['谷','洞','派','山','宗','门'])
         self.门派.append('\033[32m' + a+b + '\033[0m')
         for i in range(3):
